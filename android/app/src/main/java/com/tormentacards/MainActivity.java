74ae4ea7c7f24eca6859d1dcfb7542e22bb828bc
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-09-19 21:59:28 -0300

    chore: Disable Hermes and build

--- android/app/src/main/java/com/tormentacards/MainActivity.java
+++ android/app/src/main/java/com/tormentacards/MainActivity.java
@@ -3,6 +3,7 @@ package com.tormentacards;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "tormentaCards";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
