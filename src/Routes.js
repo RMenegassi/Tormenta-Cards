e1626691720a3f432476acfda5c49b8c49896da3
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-21 23:18:22 -0300

    feat: Routes Navigation

--- src/Routes.js
+++ src/Routes.js
@@ -2,33 +2,96 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import App from './Pages/First';
+import ClassPage from './Pages/ClassPage';
 import Schools from './Pages/Schools';
 import CardsPage from './Pages/CardsPage';
+import Favorites from './Pages/Favorites';
+import Learned from './Pages/Learned';
 
-const Routes = () => {
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const StackScreen = () => {
   const Stack = createNativeStackNavigator();
 
   return (
+    <Stack.Navigator initialRouteName="ClassPage">
+      <Stack.Screen
+        name="ClassPage"
+        component={ClassPage}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Schools"
+        component={Schools}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="CardsPage"
+        component={CardsPage}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Routes = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="First">
-        <Stack.Screen
-          name="First"
-          component={App}
-          options={{headerShown: false}}
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarInactiveBackgroundColor: 'black',
+          tabBarActiveBackgroundColor: 'black',
+        }}>
+        <Tab.Screen
+          name="Favoritos"
+          component={Favorites}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'star' : 'star-outline'}
+                size={20}
+                color={'white'}
+              />
+            ),
+            tabBarShowLabel: false,
+          }}
         />
-        <Stack.Screen
-          name="Schools"
-          component={Schools}
-          options={{headerShown: false}}
+        <Tab.Screen
+          name="Home"
+          component={StackScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'home' : 'home-outline'}
+                size={20}
+                color={'white'}
+              />
+            ),
+            tabBarShowLabel: false,
+          }}
         />
-        <Stack.Screen
-          name="CardsPage"
-          component={CardsPage}
-          options={{headerShown: false}}
+        <Tab.Screen
+          name="Aprendidas"
+          component={Learned}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'book' : 'book-outline'}
+                size={20}
+                color={'white'}
+              />
+            ),
+            tabBarShowLabel: false,
+          }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
