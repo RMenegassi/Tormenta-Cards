5690980607fd4ada124b1886e9b8a67f5c67a35b
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-21 23:19:34 -0300

    update: CardModal

--- src/components/CardModal/index.js
+++ src/components/CardModal/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import {Image} from 'react-native';
-import styled from 'styled-components';
-
-const View = styled.View`
-  position: absolute;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  z-index: 15;
-  background-color: #000000bb;
-`;
-
-const CloseButton = styled.TouchableOpacity`
-  width: 30px;
-  height: 30px;
-  border-radius: 50px;
-  background-color: #bdc3c7;
-  justify-content: center;
-  align-items: center;
-  align-self: flex-end;
-  margin: 25px 45px -20px;
-  z-index: 16;
-`;
-const CloseButtonText = styled.Text``;
+import {View, CloseButton, CloseButtonText} from './styles';
 
 const CardModal = ({visible, callback, image}) => {
   if (visible) {
@@ -33,9 +11,9 @@ const CardModal = ({visible, callback, image}) => {
         </CloseButton>
         <Image
           style={{
-            resizeMode: 'cover',
+            resizeMode: 'contain',
             width: '75%',
-            height: '80%',
+            height: '65%',
             borderRadius: 20,
             overflow: 'hidden',
             borderWidth: 3,
